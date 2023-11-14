858bae153bdbc16551b73e1927dd6514f8cbde1a
craftjohn <jjc0046@mix.wvu.edu>
2023-11-14 17:25:50 -0500

    inital commit for setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+    const todoInputRef = useRef();
+
+    const handleAddTodo = () => {
+        const newTodo = todoInputRef.current.value.trim();
+        if (newTodo !== '') {
+            setTodos((prevTodos) => [...prevTodos, newTodo]);
+            todoInputRef.current.value = '';
+        }
+    };
+
+    const handleRemoveTodo = (index) => {
+        setTodos((prevTodos) => prevTodos.filter((_, i) => i !== index));
+    };
+
+    return (
+        <div className="App">
+            <div className="todo-container">
+                <h1>To Do List</h1>
+                <div className="input-container">
+                    <input type="text" ref={todoInputRef} placeholder="Enter your todo" />
+                    <button onClick={handleAddTodo}>Add</button>
+                </div>
+                <ul className="todo-list">
+                    {todos.map((todo, index) => (
+                        <li key={index} onClick={() => handleRemoveTodo(index)}>
+                            {todo}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
